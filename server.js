5739b072bc9e1a52a3a84a3b0b2509ab2ecc8158
farruh Mirisoev <farruh.mirisoev@gmail.com>
2017-09-11 05:53:08 +0500

    integrated with slack, pickplan command works on slack!

--- server.js
+++ server.js
@@ -20,7 +20,13 @@ var router = express.Router();              // get an instance of the express Ro
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    // res.json({ message: 'hooray! welcome to our api!' });   
+    console.log("Command received")
+    res.send("List of intros1")
+});
+
+router.post('/pickplan', function(req, res) {
+	res.send("Hi sumo")
 });
 
 // more routes for our API will happen here
